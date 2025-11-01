0c2b03069f76b38623e97c7bef93d4bf32085613
Kerem Tutucu <Keremtutucu@gmail.com>
2025-11-01 12:31:15 +0100

    tweaks
    tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,7 +9,7 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
+    integrations: [react(), svelte()],
     adapter: netlify({
         devFeatures: {
             environmentVariables: true
